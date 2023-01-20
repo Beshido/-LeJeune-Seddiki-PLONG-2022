e71c13559e89c5494c567800f6560d722f115ffd
Bilal <el.famoso.remedo@gmail.com>
2023-01-20 21:26:45 +0100

    Fonction principale de détection relativement complétée.

--- board_reader.py
+++ board_reader.py
@@ -1,4 +1,4 @@
-import cv2, enum, logging
+import cv2, enum, logging, pprint
 
 CHESSBOARD_SIZE = (7, 7) # taille intérieure d'un échiquier
 DELAY = 2000 # ms
@@ -14,7 +14,6 @@ def show_image(image: cv2.Mat, delay: int = DELAY) -> None:
 
     cv2.imshow("show_image", image)
     cv2.waitKey(delay)
-    cv2.destroyAllWindows()
 
 def crop_to_square(image: cv2.Mat) -> cv2.Mat:
     height = image.shape[0]
@@ -30,33 +29,88 @@ def crop_to_square(image: cv2.Mat) -> cv2.Mat:
     return image[y1:y2, x1:x2]
 
 
-def read_chessboard(image: cv2.Mat) -> None:
-    def find_contour(image: cv2.Mat) -> cv2.Mat:
-        contours, hierarchy = cv2.findContours(image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        return cv2.drawContours(image, contours, -1, (255, 0, 0), 3)
-
-    def get_chessboard_corners_coordinates(image: cv2.Mat) -> list:
-        ret, corners = cv2.findChessboardCornersSB(image, CHESSBOARD_SIZE, None)
-        if ret:
-            logging.info(f"A chessboard was found.")
-        return corners
-
+def read_chessboard(image: cv2.Mat) -> list:
     image = crop_to_square(image)
     image_bw = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    corners = get_chessboard_corners_coordinates(image_bw)
-
+    ret, corners = cv2.findChessboardCornersSB(image_bw, CHESSBOARD_SIZE, None)
+    if not ret:
+        return []
+    
+    logging.info(f"A chessboard was found.")
+    assert(len(corners) == 49)
+    
+    coordinates = [[]]
     for i in range(len(corners) - 8):
         x1 = int(corners[i][0][0])
         y1 = int(corners[i][0][1])
         x2 = int(corners[i + 8][0][0])
         y2 = int(corners[i + 8][0][1])
 
-        if x1 > x2 or y1 > y2: continue
+        if x1 > x2 or y1 > y2: 
+            coordinates.append([])
+            continue
 
         logging.info(f"{i}. Origin point is ({x1}, {y1}). Ending point is ({x2}, {y2}). Image size is {image.shape[1]}x{image.shape[0]}.")
-        show_image(image[y1:y2, x1:x2])
+        coordinates[-1].append(((x1, y1), (x2, y2)))
+    
+    # coordonnées extérieures
+    for item in coordinates:
+        point_first = item[0]
+        point_second = item[1]
+        point_before_last = item[-2]
+        point_last = item[-1]
+
+        x1_offset_first = point_second[0][0] - point_first[0][0]
+        y1_offset_first = point_second[0][1] - point_first[0][1]
+        x2_offset_first = point_second[1][0] - point_first[1][0]
+        y2_offset_first = point_second[1][1] - point_first[1][1]
+        point0 = ((point_first[0][0] - x1_offset_first, point_first[0][1] - y1_offset_first), (point_first[1][0] - x2_offset_first, point_first[1][1] - y2_offset_first))
+        
+        x1_offset_last = point_last[0][0] - point_before_last[0][0]
+        y1_offset_last = point_last[0][1] - point_before_last[0][1]
+        x2_offset_last = point_last[1][0] - point_before_last[1][0]
+        y2_offset_last = point_last[1][1] - point_before_last[1][1]
+        point8 = ((point_last[0][0] + x1_offset_last, point_last[0][1] + y1_offset_last), (point_last[1][0] + x2_offset_last, point_last[1][1] + y2_offset_last))
+
+        item.insert(0, point0)
+        item.append(point8)
+    coordinates.insert(0, [])
+    coordinates.append([])
+    for i in range(len(coordinates)):
+        point_first = coordinates[1][i]
+        point_second = coordinates[2][i]
+        point_before_last = coordinates[-3][i]
+        point_last = coordinates[-2][i]
+
+        x1_offset_first = point_second[0][0] - point_first[0][0]
+        y1_offset_first = point_second[0][1] - point_first[0][1]
+        x2_offset_first = point_second[1][0] - point_first[1][0]
+        y2_offset_first = point_second[1][1] - point_first[1][1]
+        point0 = ((point_first[0][0] - x1_offset_first, point_first[0][1] - y1_offset_first), (point_first[1][0] - x2_offset_first, point_first[1][1] - y2_offset_first))
+
+        x1_offset_last = point_last[0][0] - point_before_last[0][0]
+        y1_offset_last = point_last[0][1] - point_before_last[0][1]
+        x2_offset_last = point_last[1][0] - point_before_last[1][0]
+        y2_offset_last = point_last[1][1] - point_before_last[1][1]
+        point8 = ((point_last[0][0] + x1_offset_last, point_last[0][1] + y1_offset_last), (point_last[1][0] + x2_offset_last, point_last[1][1] + y2_offset_last))
+
+        coordinates[0].append(point0)
+        coordinates[-1].append(point8)
+
+    for row in coordinates:
+        for point in row:
+            x1 = point[0][0]
+            y1 = point[0][1]
+            x2 = point[1][0]
+            y2 = point[1][1]
+
+            show_image(image[y1:y2, x1:x2])
+    
+    return coordinates
 
 if __name__ == "__main__":
     logging.info("Launching script...")
-    image = cv2.imread("img/chessboard-topview/image1.jpg")
+    image = cv2.imread("img/chessboard-topview/image3.webp")
     read_chessboard(image)
+
+    cv2.destroyAllWindows()
