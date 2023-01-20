064e1a439018add0ab04676592d4a4b09a55a386
Bilal <el.famoso.remedo@gmail.com>
2023-01-20 18:40:22 +0100

    Détection des cases intérieures terminé.

--- board_reader.py
+++ board_reader.py
@@ -9,29 +9,52 @@ class Color(enum.Enum):
     BLACK = 0
     WHITE = 1
 
-def show_image(image: cv2.Mat):
-    cv2.imshow("test", image)
-    cv2.waitKey(DELAY)
+def show_image(image: cv2.Mat, delay: int = DELAY) -> None:
+    if image is None: return
+
+    cv2.imshow("show_image", image)
+    cv2.waitKey(delay)
     cv2.destroyAllWindows()
 
-def read_chessboard(image: cv2.Mat) -> list:
-    def findContour(image: cv2.Mat) -> cv2.Mat:
+def crop_to_square(image: cv2.Mat) -> cv2.Mat:
+    height = image.shape[0]
+    width = image.shape[1]
+
+    size = width if height > width else height
+    x1 = width // 2 - size // 2
+    y1 = height // 2 - size // 2
+    x2 = x1 + size
+    y2 = y1 + size
+
+    logging.info(f"Origin point is ({x1}, {y1}). Ending point is ({x2}, {y2}). The size of the square is {size}.")
+    return image[y1:y2, x1:x2]
+
+
+def read_chessboard(image: cv2.Mat) -> None:
+    def find_contour(image: cv2.Mat) -> cv2.Mat:
         contours, hierarchy = cv2.findContours(image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         return cv2.drawContours(image, contours, -1, (255, 0, 0), 3)
 
-    def findChessoard(image: cv2.Mat):
-        for i in range(3, 9):
-            for j in range(3, 9):
-                ret, corners = cv2.findChessboardCornersSB(image, (i, j), None)
-                if ret:
-                    logging.info(f"A chessboard was found with ({i}, {j}).")
-                    image = cv2.drawChessboardCorners(image, (i, j), corners, ret)
-                    cv2.imshow("read_chessboard", image)
-                    cv2.waitKey(DELAY)
-        cv2.destroyAllWindows()
+    def get_chessboard_corners_coordinates(image: cv2.Mat) -> list:
+        ret, corners = cv2.findChessboardCornersSB(image, CHESSBOARD_SIZE, None)
+        if ret:
+            logging.info(f"A chessboard was found.")
+        return corners
 
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    show_image(findContour(image))
+    image = crop_to_square(image)
+    image_bw = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    corners = get_chessboard_corners_coordinates(image_bw)
+
+    for i in range(len(corners) - 8):
+        x1 = int(corners[i][0][0])
+        y1 = int(corners[i][0][1])
+        x2 = int(corners[i + 8][0][0])
+        y2 = int(corners[i + 8][0][1])
+
+        if x1 > x2 or y1 > y2: continue
+
+        logging.info(f"{i}. Origin point is ({x1}, {y1}). Ending point is ({x2}, {y2}). Image size is {image.shape[1]}x{image.shape[0]}.")
+        show_image(image[y1:y2, x1:x2])
 
 if __name__ == "__main__":
     logging.info("Launching script...")
