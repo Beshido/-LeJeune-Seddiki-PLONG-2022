de8ef820bb611e926ccfa9e6ef6cd05437cadb66
LE JEUNE alban <albanlejeune94@gmail.com>
2023-02-15 13:54:31 +0100

    Fix

--- src/chess_logic_and_tests.py
+++ src/chess_logic_and_tests.py
@@ -1,5 +1,9 @@
-import chess
+import chess.engine
 
+engine = chess.engine.SimpleEngine.popen_uci("../stockfish-11-win/Windows/stockfish_20011801_x64.exe")
 board = chess.Board()
+info = engine.analyse(board, chess.engine.Limit(time=2.0))
 
 print(board)
+print("Score:", info["score"])
+print("Best move:", info["pv"][0])
